3702078de4ebff64293b0cabd00cdf49589ea7b3
Sterlios <kuzmin.s.anton@gmail.com>
2025-03-29 13:08:43 +0100

    https://github.com/Sterlios/School_platform/issues/3 решение

--- School_Platform.Server/Program.cs
+++ School_Platform.Server/Program.cs
@@ -9,6 +9,7 @@ namespace School_Platform.Server
     {
         public static void Main(string[] args)
         {
+            //Task1
             var builder = WebApplication.CreateBuilder(args);
 
             var connectionString = builder.Configuration.GetConnectionString("SchoolDb");
