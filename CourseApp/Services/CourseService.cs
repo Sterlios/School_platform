83ed41f05ab63f80565244f86d56c46ab45ca430
Sterlios <kuzmin.s.anton@gmail.com>
2025-03-30 15:48:48 +0200

    #5 Create CourseService

---
+++ CourseApp/Services/CourseService.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseApp.Services
+{
+    public class CourseService
+    {
+        private ApplicationDbContext _context;
+
+        public CourseService(ApplicationDbContext context) =>
+            _context = context;
+    }
+}
