18a37f8dde40767515980f3f3c8a7a91dbdb5eca
Sterlios <kuzmin.s.anton@gmail.com>
2025-03-30 14:43:46 +0200

    #5 Ad common properties in models

--- CourseApp/Models/Module.cs
+++ CourseApp/Models/Module.cs
@@ -2,5 +2,9 @@
 {
     public class Module
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Lesson> Lessons { get; set; }
     }
 }
